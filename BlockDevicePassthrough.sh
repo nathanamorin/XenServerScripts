378b571be8b9219735ccdaed1a130a0c4aa2d40c
Nathan Morin <nathanamorin@users.noreply.github.com>
2017-01-19 18:58:39 -0500

    Update BlockDevicePassthrough.sh
    Added usage & Fixed bugs

--- BlockDevicePassthrough.sh
+++ BlockDevicePassthrough.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+#Usage curl https://raw.githubusercontent.com/nathanamorin/XenServerScripts/master/BlockDevicePassthrough.sh | bash -s BlockStorageName /dev/DEVICE_NAME
 STORNAME=$1
 DEVICE_NAME=$2
 
+echo $STORNAME
+
+echo $DEVICE_NAME
+
 mkdir "/srv/$STORNAME" 
-UUID = $( xe sr-create name-label=BlockStorage name-description="Block Storage Passthrough" type=udev content-type=disk device-config:location=/srv/block_storage )
+UUID=$( xe sr-create name-label=BlockStorage name-description="Block Storage Passthrough" type=udev content-type=disk device-config:location=/srv/block_storage )
 
 ln -s "$DEVICE_NAME" "/srv/$STORNAME/sdb"
 
