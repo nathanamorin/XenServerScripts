13dcd8f296b796c1f78abb8445bb617c5f74e7f7
Nathan Morin <nathanamorin@gmail.com>
2017-06-01 11:14:31 -0400

    added vApp autostart

---
+++ autostartvapps.sh
@@ -0,0 +1,41 @@
+# AutoStart XenServer vApps with the tag autostart in their description
+# Script originally created by Raido Consultants - http://www.raido.be
+# Script updated and shared by E.Y. Barthel - http://www.virtues.it
+# Comments updated and added to XenServerScripts repo by Nathan Morin - https://nathanmorin.com
+# SRC http://www.virtues.it/2014/12/step-by-step-guide-automatically-start-a-vapp-on-xenserver/
+
+# Copy this script to \opt\autostartvapps.sh
+# Make executable chmod +x \opt\autostartvapps.sh
+# Add ... to /etc/rc.d/rc.local for start on boot
+# 	sleep 40
+# 	sh /opt/autostartvapps.sh
+# Add below tag to description of vApp and vApp with start on boot
+
+
+TAG="autostart"
+ 
+# helper function
+function xe_param()
+{
+    PARAM=$1
+    while read DATA; do
+        LINE=$(echo $DATA | egrep "$PARAM")
+        if [ $? -eq 0 ]; then
+            echo "$LINE" | awk 'BEGIN{FS=": "}{print $2}'
+        fi
+    done
+} # Get all Applicances
+sleep 20
+VAPPS=$(xe appliance-list | xe_param uuid)
+for VAPP in $VAPPS
+do
+    # debug info
+    # echo "Esther's AutoStart : Checking vApp $VAPP"
+    VAPP_TAGS="$(xe appliance-param-get uuid=$VAPP param-name=name-description)"
+    if [[ $VAPP_TAGS == *$TAG* ]]; then
+        # action info:
+        echo "starting vApp $VAPP";
+        xe appliance-start uuid=$VAPP;
+        sleep 20
+    fi
+done
